ceaaa3d6df9857bf1ca69058f65bceaa7fe4260f
sanaechchadi <58998157+sanaechchadi@users.noreply.github.com>
2022-07-25 16:44:50 +0100

    Update and rename Greeter.java to Main.java

---
+++ src/main/java/com/openshift/demo/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+      int target = -5;
+      int num = 3;
+
+      target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+      target =+ num; // Noncompliant; target = 3
+      }
+}
